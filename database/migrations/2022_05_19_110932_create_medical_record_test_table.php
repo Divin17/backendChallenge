379e26814cfd801cdc349c8cf009bac578739dfd
ISHIMWE Divin Fiston <divinfiston1@gmail.com>
2022-05-19 17:04:40 +0200

    create migration

---
+++ database/migrations/2022_05_19_110932_create_medical_record_test_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_record_test', function (Blueprint $table) {
+            $table->id();
+            $table->integer('medical_record_id');
+            $table->integer('test_id');
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_record_test');
+    }
+};
